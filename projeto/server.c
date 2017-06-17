d6a625916aea26e45135edc3197894179e054ca6
LMafra <chagas.lucas.mafra@gmail.com>
2017-06-17 19:16:07 -0300

    Começando o server

--- projeto/server.c
+++ projeto/server.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <>
+#include <sys/socket.h>
+#include <sys/type.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int sockid;
+	struct sockaddr_in addrport;
+	sockid = socket(PF_INET, SOCK_STREAM, 0);
+	addrport.sin_family = AF_INET;
+	addrport.sin_port = htons(5100);
+	addrport.sin_addr.s_addr = htonl(INADDR_ANY);
+	if(bind(sockid, (struct sockaddr *) &addrport, sizeof(addrport))!= -1) {}
+}
