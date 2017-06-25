dce268a49259444b13b33fc0de896efd9fb1a760
Éden Medeiros <edenlcvm@gmail.com>
2017-06-24 23:31:20 -0300

    Servidor totalmente funcional utilizando TCP

--- projeto/common.h
+++ projeto/common.h
@@ -11,55 +11,76 @@
  */
 
 /*
- * ====== Constantes ======
+ * ================== Constantes ==================
  */
- #define MSG_SERVER_DEFAULT "A conexão foi feita com sucesso!\n"
- #define MSG_CLIENT_DEFAULT "Olá servidor!\n"
- #define ERR_PORT_RANGE     "ERRO: A porta deve ser maior que 1024 " \
-                            "e menor que 65535!\n"
- #define BUFFER_SIZE        1024
- #define QUEUE_SIZE         1
+
+#define BUFFER_SIZE 1024
+#define QUEUE_SIZE  3
+
+#define SCS_SOCKET      "-> Socket foi criado...\n"
+#define SCS_BIND        "-> Foi dado bind no socket...\n"
+#define ERR_SOCKET      "ERRO: Não foi possível criar o socket!"
+#define ERR_BIND        "ERRO: Não foi possível dar bind no socket!"
+
+#define ERR_PORT_RANGE  "ERRO: A porta deve ser maior que 1024 " \
+                         "e menor que 65535!\n"
+
+/*
+ * ---- Servidor ----
+ */
+
+#define MSG_SERVER_DEFAULT    "SERVIDOR: Digite 'quit' para encerrar a conexão!\n"
+
+#define SCS_LISTEN            "-> Esperando conexões...\n"
+#define ERR_LISTEN            "ERRO: Não foi possível escutar conexões no socket!\n"
+#define KEYWORD_STOP_SERVER   "quit"
+
+/*
+ * ---- Cliente ----
+ */
+
+#define MSG_CLIENT_DEFAULT "Olá servidor!\n"
 
 /*
  * Trata-se de uma estrutura que engloba a estrutura do socket e também
  * algumas flags.
  *
 */
- typedef struct {
-     int  socketFD;              /* File descriptor do socket de conexão */
-     bool is_Server;             /* Flag que define se é servidor */
-     bool is_TCP;                /* Flag que define se o protocolo será TCP */
-     struct sockaddr_in socket;  /* Estrutura do socket da conexão */
+typedef struct {
+    int socketFD;               /* File descriptor do socket de conexão     */
+    bool is_Server;             /* Flag que define se é servidor            */
+    bool is_TCP;                /* Flag que define se o protocolo será TCP  */
+    struct sockaddr_in socket;  /* Estrutura do socket da conexão           */
 
- } config;
+} config;
 
 /*
  * Recupera os parâmetros passados por linha de comando e retorna em uma
  * estrutura que os engloba. Caso, ocorra algum erro na recuperação dos
  * parâmetros ou esteja faltando algum parâmetro será retornado NULL.
  */
- config* recuperar_parametros(int, char**);
+config *recuperar_parametros(int, char **);
 
 /*
  * Função que abstrai a inicialização da aplicação. Nela que é decidido se o
  * programa rodará como servidor ou cliente.
  */
- void run(config*);
+void run(config *);
 
 /*
  * Rotina que faz o programa rodar como servidor.
  */
- void runAsServer(config*);
+void runAsServer(config *);
 
 /*
  * Rotina que faz o programa rodar como cliente.
  */
- void runAsClient(config*);
+void runAsClient(config *);
 
 /*
  * Rotina que apenas printa na tela quais são as configurações que o programa
  * irá rodar.
  */
- void print_config(config*);
+void print_config(config *);
 
 #endif
--- projeto/nhem3.c
+++ projeto/nhem3.c
@@ -2,79 +2,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <errno.h>
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     config *c = recuperar_parametros(argc, argv);
+
     run(c);
+
+    close(c->socketFD);
     free(c);
 }
 
-void run(config *c){
-    bool noError   = true;
-
-    if(c == NULL){
+void run(config *c) {
+    if (c == NULL) {
         printf("ERRO: Não foi possível iniciar o programar!\n"
-               "Certifique-se que:\n"
-               "-> Para servidor use:\t -S [port][-t]\n"
-               "-> Para cliente use:\t host [port][-t]\n");
+                       "Certifique-se que:\n"
+                       "-> Para servidor use:\t -S [port][-t]\n"
+                       "-> Para cliente use:\t host [port][-t]\n");
 
         return;
     }
 
     print_config(c);
-    if(noError){
-        if(c->is_Server){
-            runAsServer(c);
+
+    c->socketFD = socket(AF_INET, c->is_TCP ? SOCK_STREAM : SOCK_DGRAM, 0);
+
+    if (c->socketFD == -1) {
+        // workaround: Quando o servidor roda a partir da segunda vez, o socket pode não ter
+        // sido liberado pelo OS ou se encontra em WAIT. Dessa maneira, tenta-se reutilizá-lo.
+        if (c->is_Server) {
+            setsockopt(c->socketFD, SOL_SOCKET, SO_REUSEADDR, (const void *) 1, sizeof(int));
         } else {
-            runAsClient(c);
+            printf("%s, código: %d", ERR_SOCKET, errno);
+            return;
         }
+    } else {
+        printf(SCS_SOCKET);
+    }
+
+    if (c->is_Server) {
+        runAsServer(c);
+    } else {
+        runAsClient(c);
     }
 }
 
-void runAsServer(config* c){
-    // TODO: Mudar para aceitar tanto TCP quanto UDP.
+void runAsServer(config *c) {
     int clientFD;
     char mensagem[BUFFER_SIZE];
     struct sockaddr_in client;
     socklen_t socketSize = sizeof(client);
 
-    c->socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (bind(c->socketFD, (struct sockaddr *) &c->socket, sizeof(c->socket)) == -1) {
+        printf("%s código: %d\n", ERR_BIND, errno);
+        return;
+    } else {
+        printf(SCS_BIND);
+    }
 
-    bind(c->socketFD, (struct sockaddr *) &c->socket, sizeof(c->socket));
-    listen(c->socketFD, QUEUE_SIZE);
-
-    // recvfrom(c->socketFD, (void *) &mensagem, 1024, 0,
-    // (struct sockaddr *) &c->socket, sizeof(c->socket));
+    if (listen(c->socketFD, QUEUE_SIZE) == -1) {
+        printf(ERR_LISTEN);
+        return;
+    } else {
+        printf(SCS_LISTEN);
+    }
 
     clientFD = accept(c->socketFD, (struct sockaddr *) &client, &socketSize);
 
-    printf("Conexão recebida: %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+    printf("Servidor recebeu uma conexão de %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 
     write(clientFD, MSG_SERVER_DEFAULT, sizeof(MSG_SERVER_DEFAULT));
+
+    while (1) {
+        bzero(mensagem, BUFFER_SIZE);
+        read(clientFD, mensagem, BUFFER_SIZE);
+        mensagem[strlen(mensagem) - 2] = '\0';  // remover \r \n da string
+
+        printf("[%s:%d] : %s\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port), mensagem);
+
+        if (strcmp(KEYWORD_STOP_SERVER, mensagem) == 0) {
+            printf("Conexão encerrada por %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+            break;
+        }
+    }
+
 
     close(clientFD);
 }
 
-void runAsClient(config* c){
+void runAsClient(config *c) {
     // TODO: Mudar para aceitar tanto TCP quanto UDP.
     int clientFD, clientSock;
     char mensagem[BUFFER_SIZE];
     struct sockaddr_in server;
     socklen_t socketSize = sizeof(c->socket);
 
-    clientSock = c->socketFD;
-
-    clientSock = socket(AF_INET, SOCK_STREAM, 0);
-
-
-    clientFD = connect(clientSock, (struct sockaddr *) &c->socket,  &socketSize);
+    clientFD = connect(c->socketFD, (struct sockaddr *) &c->socket, socketSize);
 
     printf("Olá, você está conectado \n");
 
-    read(clientSock, MSG_CLIENT_DEFAULT, sizeof(MSG_CLIENT_DEFAULT));
+    read(c->socketFD, MSG_CLIENT_DEFAULT, sizeof(MSG_CLIENT_DEFAULT));
 
     printf("Conexão recebida: %s:%d\n", inet_ntoa(server.sin_addr), ntohs(server.sin_port));
 
@@ -82,29 +111,29 @@ void runAsClient(config* c){
     close(clientFD);
 }
 
-config* recuperar_parametros(int counter, char** params){
+config *recuperar_parametros(int counter, char **params) {
 
     int portNumber;
 
     config *ptr = (config *) malloc(sizeof(config));
 
-    if(ptr == NULL){
+    if (ptr == NULL) {
         return ptr;
     }
 
-    ptr->is_Server         = strcmp("-S", params[1]) == 0 ? true : false;
+    ptr->is_Server = strcmp("-S", params[1]) == 0 ? true : false;
     ptr->socket.sin_family = AF_INET;
-    ptr->socket.sin_port   = htons(33333);
+    ptr->socket.sin_port = htons(33333);
 
     // Recuperar parâmetros opcionais -t e/ou [porta].
-    if( counter > 2 ){
-        for( int i = 2; i < counter; i++ ){
+    if (counter > 2) {
+        for (int i = 2; i < counter; i++) {
 
             // Foi passado um argumento númerico.
             // Possivelmente é o número da porta.
-            if( (portNumber = atoi(params[i])) > 0 ){
+            if ((portNumber = atoi(params[i])) > 0) {
 
-                if( portNumber < 1024 || portNumber > 65535 ){
+                if (portNumber < 1024 || portNumber > 65535) {
                     printf(ERR_PORT_RANGE);
                     free(ptr);
                     return NULL;
@@ -119,15 +148,15 @@ config* recuperar_parametros(int counter, char** params){
     }
 
     ptr->socket.sin_addr.s_addr = ptr->is_Server ? INADDR_ANY
-                                : inet_addr(params[1]); // FIXME: e se não for
-                                                        // um ip válido ?!
+                                                 : inet_addr(params[1]); // FIXME: e se não for
+    // um ip válido ?!
     return ptr;
 }
 
-void print_config(config *c){
+void print_config(config *c) {
     printf("Inicializando programa como: %s\n"
-           "IP: %s, Porta: %d\n"
-           "Protocolo: %s\n\n",
+                   "IP: %s, Porta: %d\n"
+                   "Protocolo: %s\n\n",
            c->is_Server ? "Servidor" : "Cliente",
            inet_ntoa(c->socket.sin_addr),
            ntohs(c->socket.sin_port),
